b641494e2f8f79e7c5edd83ecd673ec0e3412f20
sqc <56352094+key-sqc@users.noreply.github.com>
2026-02-09 12:48:35 +0800

    Add files via upload

--- projects/ChickIn/models/Checkin.js
+++ projects/ChickIn/models/Checkin.js
@@ -1,1 +1,15 @@
+const mongoose = require('../mongoose.js');
 
+const CheckinSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    content: { type: String, required: true },
+    date: { type: String, required: true, match: /^\d{4}-\d{2}-\d{2}$/ },
+    isSupplement: { type: Boolean, default: false },
+    targetDate: { type: String, default: null, match: /^\d{4}-\d{2}-\d{2}$/ }
+}, {
+    timestamps: true,
+    collection: 'checkins'
+});
+
+const Checkin = mongoose.model('Checkin', CheckinSchema);
+module.exports = Checkin;
